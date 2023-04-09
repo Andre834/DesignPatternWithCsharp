aa8681026ef30a782cee49b9a5d2d806115b0a55
Andre Santos <63032209+Andre834@users.noreply.github.com>
2023-04-09 02:44:10 -0300

    Add project files.

---
+++ ConceptsPrototype/ConcretePrototypeA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPrototype
+{
+    public class ConcretePrototypeA : IPrototype
+    {
+        public IPrototype Clone()
+        {
+            // Shallow Copy: only top-level objects are duplicated
+            return (IPrototype)MemberwiseClone();
+
+            // Deep Copy: all objects are duplicated
+            //return (Prototype)this.Clone();
+        }
+       
+    }
+
+    public class ConcretePrototypeB : IPrototype
+    {
+        public IPrototype Clone()
+        {
+            // Shallow Copy: only top-level objects are duplicated
+            return (IPrototype)MemberwiseClone();
+
+            // Deep Copy: all objects are duplicated
+            //return (Prototype)this.Clone();
+        }
+    }
+}
---
+++ ConceptsPrototype/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Numerics;
+
+namespace ConceptsPrototype
+{
+    /// <summary>
+    /// Prototype Pattern Demo
+    /// </summary>
+
+// O que é Padrão Protótipo?
+// O padrão de design de protótipo é usado para criar um objeto duplicado ou clone do objeto atual para aprimorar o desempenho.
+// Esse padrão é usado quando a criação de um objeto é cara ou complexa.
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Developer dev = new Developer();
+            dev.Name = "Rahul";
+            dev.Role = "Team Leader";
+            dev.PreferredLanguage = "C#";
+
+            Developer devCopy = (Developer)dev.Clone();
+            devCopy.Name = "Arif"; //Not mention Role and PreferredLanguage, it will copy above
+
+            Console.WriteLine(dev.GetDetails());
+            Console.WriteLine(devCopy.GetDetails());
+
+            Typist typist = new Typist();
+            typist.Name = "Monu";
+            typist.Role = "Typist";
+            typist.WordsPerMinute = 120;
+
+            Typist typistCopy = (Typist)typist.Clone();
+            typistCopy.Name = "Sahil";
+            typistCopy.WordsPerMinute = 115;//Not mention Role, it will copy above
+
+            Console.WriteLine(typist.GetDetails());
+            Console.WriteLine(typistCopy.GetDetails());
+
+            Console.ReadKey();
+
+        }
+    }
+}
